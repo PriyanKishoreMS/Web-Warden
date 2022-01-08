a93156caeb5821899d15eeedcda7528fd39c63a8
Sreeram KL <45222269+Geeky-cat@users.noreply.github.com>
2022-01-08 20:35:53 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,35 +1,96 @@
 // Replace text
-// content=document.body.innerHTML;
-// content=content.replaceAll("Sreeram","Haha Gotcha")
-// document.body.innerHTML=content
-// console.log(content)
+content=document.body.innerHTML;
+for(i in offensive_wds){
+    content=content.replaceAll(offensive_wds[i]+" "," Haha Gotcha")
+    content=content.replaceAll(" "+offensive_wds[i]," Haha Gotcha")
+}
 
-document.getElementById("toggle").style.display = "none";
 
 // Replace images
 
-// body = document.body.innerHTML;
-// var parser = new DOMParser();
-// var doc = parser.parseFromString(body, "text/html");
-// var imgs = doc.getElementsByTagName("img");
-// images = [];
-// for (i = 0; i < imgs.length; i++) {
-// 	images.push(imgs[i].src);
-// }
+body = content
+var parser = new DOMParser();
+var doc = parser.parseFromString(body, "text/html")
+var imgs =doc.getElementsByTagName("img");
+images=[]
+for(i=0;i<imgs.length;i++){
 
-// data = { urls: images };
-// document.body.innerHTML = doc.body.innerHTML;
+    images.push(imgs[i].src)
+}
+data={"urls":images}
 
-// fetch("http://165.232.183.113/", {
-// 	method: "POST",
-// 	body: JSON.stringify(data),
-// 	headers: {
-// 		"Content-Type": "application/json",
-// 	},
-// })
-// 	.then(function (res) {
-// 		return res.json();
-// 	})
-// 	.then(function (data) {
-// 		alert(JSON.stringify(data));
-// 	});
+
+// Checking state
+chrome.storage.local.get(["state"], function(items){
+    if(items.state=="on"){
+        chrome.storage.local.get(["image"], function(items){
+            if(items.image=="on"){
+                FilterImage();
+            }
+          });
+          chrome.storage.local.get(["text"], function(items){
+            if(items.text=="on")
+                finish();
+          });
+    }
+    else{
+        document.getElementsByTagName("html")[0].style.display="block";
+    }
+  });
+
+
+
+// End of replace images
+function mainFunction(data){
+    replaceable=[];
+    for (key in data){
+        keyIndex=key.lastIndexOf("/");
+        urlKey=key.substring(keyIndex+1);
+        urlKey=urlKey.replaceAll("_","/");
+        urlKey=atob(urlKey);
+        if(data[key].unsafe<=data[key].safe){ // >= gives wrong result , else proper one
+            console.log(urlKey+" ==> Safe");
+        }
+        else{
+            replaceable.push(urlKey);
+            console.log(urlKey+" ==> Unsafe");
+        }
+    }
+
+    if(replaceable.length>0){
+        for(i=0;i<imgs.length;i++){
+            console.log(imgs[i].src)
+            if(replaceable.includes(imgs[i].src)){
+                console.log("replaced")
+                doc.getElementsByTagName("img")[i].src="https://i.stack.imgur.com/fbIWO.jpg?s=64&g=1";
+            }
+        }
+    }
+
+    finish()
+
+}
+
+
+
+function FilterImage(){
+    fetch("http://165.232.183.113/",
+    {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(function(res){ return res.json(); })
+    .then(function(data){ mainFunction(data) })
+
+}
+//{"url":{"unsafe":0.9,"safe":0.2},}
+
+function finish(){
+        //Place HTML after replacing text and HTML
+        console.log(doc)
+        document.body.innerHTML=doc.body.innerHTML.toString()
+        document.getElementsByTagName("html")[0].style.display="block";
+}
